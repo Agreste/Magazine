ee2b9cb86aa5531eba8886c2929456f3b8baed91
eduardoliron <eduardoliron@Edu.local>
2016-03-28 18:08:15 -0300

    teste

--- static/javascripts/article.js
+++ static/javascripts/article.js
@@ -100,5 +100,5 @@ if(!$("html").hasClass("touch")){
 
 var c=0;
 $(".jumper").click(function(){
-    $("body, html").animate({left: ++c%2*100 }, 'fast');
+    $("body, html").stop().animate({left: ++c%2*100 }, 'fast');
 });

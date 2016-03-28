d533386a97e555df4a0636abd54996842e6099b2
eduardoliron <eduardoliron@Edu.local>
2016-03-28 18:14:26 -0300

    testando

--- static/javascripts/article.js
+++ static/javascripts/article.js
@@ -100,5 +100,5 @@ if(!$("html").hasClass("touch")){
 
 var c=0;
 $(".jumper").click(function(){
-    $("body, html").stop().animate({left: ++c%2*100 }, 'fast');
+    $("body, html").animate({ scrollLeft: $( $(this).attr('href') ).offset().left}, 'fast');
 });
